2c8efa079905168654e1b332c075317a93777234
Mariusz Rajczakowski <mariuszrajczakowski@Mariuszs-MacBook-Pro.local>
2016-10-02 19:08:54 +0100

    fixed semantic directory, semantic build is now invoked once global gulp is called

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,20 +1,17 @@
+import $ from 'jquery';
+import jQuery from 'jquery';
+// export for others scripts to use
+window.$ = $;
+window.jQuery = jQuery;
 
-/**
- * First we will load all of this project's JavaScript dependencies which
- * include Vue and Vue Resource. This gives a great starting point for
- * building robust, powerful web applications using Vue and Laravel.
- */
 
-require('./bootstrap');
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the body of the page. From here, you may begin adding components to
- * the application, or feel free to tweak this setup for your needs.
- */
+$(document)
+  .ready(function() {
+    $('.ui.dropdown').dropdown();
 
-Vue.component('example', require('./components/Example.vue'));
-
-const app = new Vue({
-    el: 'body'
-});
+    $('.ui.buttons .dropdown.button').dropdown({
+      action: 'combo'
+    });
+  })
+;
--- resources/views/layouts/homepage.blade.php
+++ resources/views/layouts/homepage.blade.php
@@ -6,7 +6,7 @@
   <meta charset="utf-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
-  <link rel="stylesheet" type="text/css" href="semantic/dist/semantic.css">
+  <link rel="stylesheet" type="text/css" href="semantic/semantic.min.css">
   <title>App Name - @yield('title')</title>
 </head>
 <body>
@@ -16,6 +16,6 @@
 
   </div>
   <script src="js/app.js"></script>
-  <script src="semantic/dist/semantic.js"></script>
+  <script src="semantic/semantic.min.js"></script>
 </body>
 </html>
