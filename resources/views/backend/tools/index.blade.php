6653484b6955e522569b84cda9114bf338296c52
Mariocoski <mariuszrajczakowski@gmail.com>
2016-11-22 18:39:33 +0000

    sketched tools view

--- resources/views/backend/tools/index.blade.php
+++ resources/views/backend/tools/index.blade.php
@@ -2,8 +2,13 @@
 
 @section('title', 'Page Title')
 
-
-
 @section('content')
-  <h1>Content</h1>
+  <h2>Tools</h2>
+  <h3>Maintenance Mode</h3>
+
+  <h3>Reset Search Engine Index</h3>
+
+  <h3>Clear cache</h3>
+
+  <h3>Make database backup</h3>
 @endsection
