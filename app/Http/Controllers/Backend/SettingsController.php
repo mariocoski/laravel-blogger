b1df8a8b4429c9f100bbd8044eadce4a54bdf156
Mariocoski <mariuszrajczakowski@gmail.com>
2016-11-23 23:11:27 +0000

    sketching settings index page

--- app/Http/Controllers/Backend/SettingsController.php
+++ app/Http/Controllers/Backend/SettingsController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use View;
 
 class SettingsController extends Controller
 {
     public function index()
     {
-        return View::make('backend.settings.index');
+        $user = User::find(1);
+
+        return View::make('backend.settings.index', compact('user'));
     }
+
 }
