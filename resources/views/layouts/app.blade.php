09b43716586b2a143fe296e4817e9f2979bca310
Mariocoski <mariuszrajczakowski@gmail.com>
2017-01-11 21:26:51 +0000

    fix js bootstraping

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,3 +1,5 @@
+<!DOCTYPE html>
+<html>
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -5,31 +7,31 @@
     <link rel="icon" href="favicon.ico" type="image/x-icon" />
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
     <!-- Styles -->
     <link rel="stylesheet" type="text/css" href="/semantic/semantic.css">
-    <link href="/css/app.css" rel="stylesheet">
-
+    <link href="{{ url('css/app.css') }}" rel="stylesheet">
+     <!-- Scripts -->
+    <script src="{{ url('js/app.js') }}" ></script>
+    <script src="{{ url('semantic/semantic.js') }}" ></script>
     <title>{{ config('app.name', 'Blogger') }} - @yield('title')</title>
-    <!-- Scripts -->
 
 </head>
-    <body>
-      @include('partials._nav_sidebar')
-      <div class="pusher">
-        <div class="main-content">
-        @include('partials._nav_top')
-        @include('partials._breadcrumbs')
+<body>
+  @include('partials._nav_sidebar')
 
-          @yield('content')
-        <div>
-        @include('partials._footer')
-        @include('partials._contact_form_modal')
-        @include('partials._scroll_top')
-      </div><!--end of pusher-->
-    <script src="/js/app.js"></script>
-    <script src="/semantic/semantic.js"></script>
-      @yield('scripts')
+  <div class="pusher">
+    <div class="main-content">
+      @include('partials._nav_top')
+      @include('partials._breadcrumbs')
 
-    </body>
-  </html>
+      @yield('content')
+    </div>
+    @include('partials._footer')
+    @include('partials._contact_form_modal')
+    @include('partials._scroll_top')
+  </div><!--end of pusher-->
+
+  @yield('scripts')
+
+</body>
+</html>
